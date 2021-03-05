312f2e7695cf66467b9778dc9231e3ed6ec64f27
Viktoria Ferstl <hello@viktoriamf.com>
2021-03-05 11:58:02 +0100

    refactor(Book): add isFavorite function; define fillable columns

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -13,4 +13,15 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    // define which properties are writable (NOT: id, created_at, updated_at)
+    protected $fillable = ['isbn', 'title', 'subtitle', 'published', 'rating', 'description', 'user_id'];
+
+    /*
+     * TEST WITH TINKER (CLI): php artisan tinker
+     * >>> App\Models\Book::first()->isFavorite();
+     */
+    public function isFavorite() : bool {
+    	return $this->rating >= 8;
+	}
 }
