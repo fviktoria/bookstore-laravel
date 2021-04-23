575f435d2c64cc01094923a5941de2c0d472e706
Viktoria <hello@viktoriamf.com>
2021-04-23 11:26:01 +0200

    implement authentication (jwt)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -16,8 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('auth/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
+});
+
+Route::group(['middleware' => ['api', 'auth.jwt']], function() {
+    Route::post('book', [BookController::class, 'save']);
+    Route::put('book/{isbn}', [BookController::class, 'update']);
+    Route::delete('book/{isbn}', [BookController::class, 'delete']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
 });
 
 Route::get('books', [BookController::class, 'index']);
@@ -28,8 +38,4 @@ Route::get('book/checkisbn/{isbn}', [BookController::class,'checkISBN']);
 Route::get('books/search/{searchTerm}', [BookController::class,'findBySearchTerm']);
 
 
-Route::post('book', [BookController::class, 'save']);
 
-Route::put('book/{isbn}', [BookController::class, 'update']);
-
-Route::delete('book/{isbn}', [BookController::class, 'delete']);
